de0d886f406c796289c283e5522993395ffbb133
Beyalghozy29 <muhammad.2903@students.amikom.ac.id>
2020-04-11 12:10:53 +0700

    inisialisasi Project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Bey Alghozy", 21);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Bey Alghozy", 21, "19.11.2640", "muhammad.2903@students.@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
